2b30bab99dae8bebaac67d0d104b102eab41ba1d
Thimira-Hansana <148532453+Thimira-Hansana@users.noreply.github.com>
2026-03-09 20:02:38 +0530

    Add color js file

---
+++ furniture-visualizer/src/member 3/color.js
@@ -0,0 +1,24 @@
+﻿import { clamp } from '../member 2/clamp'
+
+const hexToRgb = (hex) => {
+  const cleaned = hex.replace('#', '')
+  if (cleaned.length !== 6) return { r: 0, g: 0, b: 0 }
+  const num = parseInt(cleaned, 16)
+  return {
+    r: (num >> 16) & 255,
+    g: (num >> 8) & 255,
+    b: num & 255,
+  }
+}
+
+export const shadeColor = (hex, shade) => {
+  const { r, g, b } = hexToRgb(hex)
+  const intensity = clamp(shade, 0, 0.85)
+  const shaded = {
+    r: Math.round(r * (1 - intensity)),
+    g: Math.round(g * (1 - intensity)),
+    b: Math.round(b * (1 - intensity)),
+  }
+  return `rgb(${shaded.r}, ${shaded.g}, ${shaded.b})`
+}
+
